bd9990d23e9ca6e78f90965bf1cb1d7153c1c0f9
Vivek Ranjan <infovivek.ranjan@gmail.com>
2022-04-04 18:58:56 +0530

    vercel deploy test

--- server/index.js
+++ server/index.js
@@ -1,14 +1,14 @@
 const express = require("express");
-const connect = require("./config/db");
+const connect = require("./src/config/db");
 const app = express();
 app.use(express.json());
 
 const PORT = process.env.PORT || 5000;
 
-const studentDetailsController = require("./controller/studentDetails.controller");
+const studentDetailsController = require("./src/controller/studentDetails.controller");
 app.use("/studentDetails", studentDetailsController);
 
-const studentResultController = require("./controller/studentResult.controller");
+const studentResultController = require("./src/controller/studentResult.controller");
 app.use("/studentResults", studentResultController);
 
 app.listen(PORT, async (req, res) => {
